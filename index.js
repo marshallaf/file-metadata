cb56fa0c8954a87a84b4e4314cd2c26d3144240b
Andrew Marshall <marshall.a.f@gmail.com>
2017-04-08 16:17:28 -0500

    added upload logic

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const fileupload = require('express-fileupload');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 8080));
+
 app.use(fileupload());
 
-app.use(express.static('/'));
+app.use(express.static(__dirname));
 
 app.post('/upload', (req, res) => {
+  if (!req.files || !req.files.fileUpload) {
+    return res.send({ error: 'no files uploaded' });
+  }
 
+  metadata = {};
+  metadata.name = req.files.fileUpload.name;
+  metadata.mimetype = req.files.fileUpload.mimetype;
+  metadata.filesize = req.files.fileUpload.data.length;
+  
+  res.send(metadata);
 });
+
+app.listen(app.get('port'), () => console.log('listening on port', app.get('port')));
